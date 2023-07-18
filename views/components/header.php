b9cb51e021aa07efd65010c5a75226d6e58d18c6
a2kad <reshetnikovruslan705@gmail.com>
2023-07-18 17:05:54 +0200

    front footer sticy

--- views/components/header.php
+++ views/components/header.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" class="h-100">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <title>Web App</title>
+
 </head>
-<body>
+
+<body class="d-flex flex-column h-100">
+    <header>
+
+    </header>
